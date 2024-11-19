a4b965942d963578cc653cf5d170f104443a665f
TankNguyn <haithanh040305@gmail.com>
2024-11-19 16:06:11 +0700

    week9 completed

--- src/Week9/HashTable.cpp
+++ src/Week9/HashTable.cpp
@@ -1,8 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-struct Node{
+struct Node
+{
     string key;
     int value;
     Node *next;
@@ -12,33 +13,43 @@ struct Node{
 
 typedef struct Node *point;
 
-class HashTable{
+class HashTable
+{
 private:
     point HT[1000];
-    
-    int HashCode(string k){
-        int s=0;
-        for(int i=0; i<k.length(); i++){
+
+    int HashCode(string k)
+    {
+        int s = 0;
+        for (int i = 0; i < k.length(); i++)
+        {
             s = s + k[i];
         }
         return s % 100;
     }
+
 public:
-    HashTable(){
-        for(int i=0; i<100; i++){
+    HashTable()
+    {
+        for (int i = 0; i < 100; i++)
+        {
             HT[i] = NULL;
         }
     }
-    void put(string k, int val){
+    void put(string k, int val)
+    {
         int tmp = HashCode(k);
-        point x = new Node(k,val);
-        if(HT[tmp] == NULL){
+        point x = new Node(k, val);
+        if (HT[tmp] == NULL)
+        {
             HT[tmp] = x;
             return;
         }
         point y = HT[tmp];
-        while(y->next != NULL){
-            if(y->key == k){
+        while (y->next != NULL)
+        {
+            if (y->key == k)
+            {
                 y->value = val;
                 return;
             }
@@ -47,48 +58,58 @@ public:
         y->next = x;
     }
 
-    int get(string k){
+    int get(string k)
+    {
         int tmp = HashCode(k);
         point x = HT[tmp];
-        while(x != NULL){
-            if(x->key == k) return x->value;
+        while (x != NULL)
+        {
+            if (x->key == k)
+                return x->value;
             x = x->next;
         }
         return -1;
     }
 
-    void del(string k){
+    void del(string k)
+    {
         int tmp = HashCode(k);
-        if(HT[tmp] == NULL) return;
-        if(HT[tmp]->next == NULL){
-            if(HT[tmp]->key == k) HT[tmp] = NULL;
+        if (HT[tmp] == NULL)
+            return;
+        if (HT[tmp]->next == NULL)
+        {
+            if (HT[tmp]->key == k)
+                HT[tmp] = NULL;
             return;
         }
         point x = HT[tmp], y = HT[tmp];
-        while(y->key != k){
-            if(y->next == NULL) return;
+        while (y->key != k)
+        {
+            if (y->next == NULL)
+                return;
             y = y->next;
-            if(x != HT[tmp]) x = x->next;
+            if (x != HT[tmp])
+                x = x->next;
         }
         x->next = y->next;
         y->next = NULL;
     }
 };
 
-int main(){
+int main()
+{
     HashTable a;
-    a.put("Lemon",5);
-    a.put("Banana",6);
-    a.put("nomeL",5);
-    a.put("Apple",10);
-    cout<<a.get("Apple")<<endl;
+    a.put("Lemon", 5);
+    a.put("Banana", 6);
+    a.put("nomeL", 5);
+    a.put("Apple", 10);
+    cout << a.get("Apple") << endl;
     a.del("Apple");
-    cout<<a.get("nomeL")<<endl;
+    cout << a.get("nomeL") << endl;
     a.del("nomeL");
-    cout<<a.get("Banana")<<endl;
+    cout << a.get("Banana") << endl;
     a.del("Banana");
-    cout<<a.get("Lemon")<<endl;
+    cout << a.get("Lemon") << endl;
     a.del("Lemon");
     return 0;
 }
-
---
+++ src/Week9/LinearProbling.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    string key;
+    int value;
+
+    Node(string k, int val) : key(k), value(val) {};
+};
+
+typedef Node *point;
+
+class LinearProbling{
+private:
+    point lp[10000];
+
+    int HashCode(string k){
+        int s = 0;
+        for(int i=0; i<k.length(); i++){
+            s = s +k[i];
+        }
+        return s%100;
+    }
+
+public:
+    LinearProbling(){
+        for(int i=0; i<10000; i++){
+            lp[i] = NULL;
+        }
+    }
+
+    void put(string k, int val){
+        int tmp = HashCode(k);
+        while(lp[tmp] != NULL){
+            if(lp[tmp]->key == "\0" || lp[tmp]->key==k) break;
+            else tmp++;
+        }
+        if(lp[tmp] == NULL) lp[tmp] = new Node(k,val);
+        else{
+            lp[tmp]->key=k;
+            lp[tmp]->value=val;
+        }
+    }
+
+    int get(string k){
+        int tmp = HashCode(k);
+        while(lp[tmp] != NULL){
+            if(lp[tmp]->key==k) return lp[tmp]->value;
+            tmp++;
+        }
+        return -1;
+    }
+
+    void del(string k){
+        int tmp = HashCode(k);
+        while(lp[tmp] != NULL){
+            if(lp[tmp]->key==k) break;
+            else tmp++;
+        }
+        if(lp[tmp]==NULL) return;
+        lp[tmp]->key="\0";
+    }
+};
+
+int main(){
+    LinearProbling a;
+    a.put("Lemon", 5);
+    a.put("Banana", 6);
+    a.put("nomeL", 5);
+    a.put("Apple", 10);
+    cout << a.get("Apple") << endl;
+    a.del("Apple");
+    cout << a.get("nomeL") << endl;
+    a.del("nomeL");
+    cout << a.get("Banana") << endl;
+    a.del("Banana");
+    cout << a.get("Lemon") << endl;
+    a.del("Lemon");
+    return 0;
+}
